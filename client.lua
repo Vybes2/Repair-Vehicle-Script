15859c0c6d8ea58c1a6f810e294bb1b23b3a5a2d
Tony Vee <110684216+Vybes2@users.noreply.github.com>
2023-12-16 22:25:24 -0500

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,18 +1,29 @@
+local waitTime = 0
+
 RegisterCommand("repair", function(source, args)
+    local ped = GetPlayerPed(-1)
+    
+    if IsPedInAnyVehicle(ped, false) then
+        local currentVehicle = GetVehiclePedIsIn(ped, false)
+        local vehicleBodyHealth = GetVehicleBodyHealth(currentVehicle)
+        local vehicleEngineHealth = GetVehicleEngineHealth(currentVehicle)
 
-local ped = GetPlayerPed(-1)
-local currentVehicle = GetVehiclePedIsIn(ped, false)
-local vehicleBodyHealth = GetVehicleBodyHealth(currentVehicle)
-local vehicleEngineHealth = GetVehicleEngineHealth(currentVehicle)
+        if vehicleBodyHealth <= 500 and vehicleEngineHealth <= 500 then
+            waitTime = 10000
 
-if IsPedInAnyVehicle(ped) then
-    SetVehicleBodyHealth(currentVehicle, 1000.0)
-    SetVehicleEngineHealth(currentVehicle, 1000.0)
-    SetVehicleFixed(currentVehicle)
-    SetVehicleDeformationFixed(currentVehicle)
+            Citizen.Wait(waitTime)
 
-end
-end, false
-)
+            SetVehicleBodyHealth(currentVehicle, 1000.0)
+            SetVehicleEngineHealth(currentVehicle, 1000.0)
+            SetVehicleFixed(currentVehicle)
+            SetVehicleDeformationFixed(currentVehicle)
+        else
+            TriggerEvent("chat:addMessage", { args = {"^1Error", "Your vehicle is not damaged enough to be repaired."} })
+        end
+    else
+        TriggerEvent("chat:addMessage", { args = {"^1Error", "You are not in a vehicle."} })
+    end
+end)
 
 TriggerEvent("chat:addSuggestion", "/repair", "Repairs your vehicle")
+TriggerEvent("chat:addMessage", "You must wait " .. waitTime/1000 .. " seconds before your car is repaired.")
